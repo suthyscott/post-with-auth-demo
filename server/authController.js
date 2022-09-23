a76fb93bdec78464df52f0fecd29d4bd474310f2
suthyscott <sutherlandscott21@gmail.com>
2022-09-23 09:58:14 -0600

    added login functionality

--- server/authController.js
+++ server/authController.js
@@ -26,12 +26,19 @@ module.exports = {
 
         if(user){
             console.log("it's a login")
+            const authenticated = bcrypt.compareSync(password, user.password)
+
+            if(authenticated){
+                res.status(200).send({username: user.username, firstName: user.first_name, lastName: user.last_name, userId: user.user_id})
+            } else {
+                res.status(401).send('wrong password')
+            }
         } else if(!user && firstName && lastName){
-            console.log("it's a register")
+            // console.log("it's a register")
 
             const salt = bcrypt.genSaltSync(5)
             const passHash = bcrypt.hashSync(password, salt)
-            console.log(salt, passHash)
+            // console.log(salt, passHash)
 
             const [[newUser]] = await sequelize.query(`
                 INSERT INTO users ( username, first_name, last_name, password ) 
@@ -39,7 +46,7 @@ module.exports = {
                 RETURNING user_id, username, first_name, last_name;
             `)
 
-            console.log(newUser)
+            // console.log(newUser)
             res.status(200).send(newUser)
         }
     }
--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [login, setLogin] = useState(false)
+  const [username, setUsername] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [password, setPassword] = useState('')
+  const [welcomeBanner, setWelcomeBanner] = useState('')
+
+  const authenticate = (e) => {
+    e.preventDefault()
+    console.log('hit')
+    axios.post('http://localhost:4546/user', {username, firstName, lastName, password})
+      .then(res => setWelcomeBanner(`Welcome, ${res.data.username}`))
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {login ? <button onClick={() => setLogin(!login)}>Register</button> : <button onClick={() => setLogin(!login)}>Login</button>}
+      {login ? (
+        <form onSubmit={(e) => authenticate(e)}>
+        <input placeholder='username' onChange={e => setUsername(e.target.value)}/>
+        <input placeholder='password' onChange={e => setPassword(e.target.value)}/>
+        <button>Submit</button>
+
+      </form>
+      ) : (
+        <form onSubmit={(e) => authenticate(e)}>
+        <input placeholder='username'onChange={e => setUsername(e.target.value)}/>
+        <input placeholder='first name'onChange={e => setFirstName(e.target.value)}/>
+        <input placeholder='last name'onChange={e => setLastName(e.target.value)}/>
+        <input placeholder='password' onChange={e => setPassword(e.target.value)}/>
+        <button>Submit</button>
+      </form>
+      )}
+
+        <h1>{welcomeBanner}</h1>
     </div>
   );
 }
